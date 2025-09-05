d39aee20d63ecba62b29e3f66c2039b4a0f95b7e
Hrishita Babaria <hrishitababaria08@gmail.com>
2025-09-05 16:45:17 -0400

    week1_activity.py

--- week1_activity.py
+++ week1_activity.py
@@ -95,7 +95,7 @@ def matrix_multiply_loop(mat1, mat2):
     for i in range(len(mat1)):
       for j in range(len(mat2[0])):
         for k in range(len(mat2)):
-          result[i][j]=+ mat1[i][k] * mat2[k][j]
+          result[i][j]+= mat1[i][k] * mat2[k][j]
 
     return result
     pass
